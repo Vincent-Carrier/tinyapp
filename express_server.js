e532dce616d57489ba1543cf03f3b31d580a27b4
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-12-09 17:34:41 -0500

    Implement generateRandomString

--- express_server.js
+++ express_server.js
@@ -5,6 +5,13 @@ const path = require('path');
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(0, 6);
+}
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,9 +25,18 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  res.end();
 });
 
 app.get("/urls/:shortURL", (req, res) => {
