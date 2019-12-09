34bacd530c33329ce3cd85783993526e9d38b92c
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-12-09 16:48:37 -0500

    Add header partial

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
+const path = require('path');
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -13,6 +16,17 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const key = req.params.shortURL;
+  let templateVars = { shortURL: key, longURL: urlDatabase[key] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
